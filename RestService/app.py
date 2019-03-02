b844c6efb1bb62600cdea109f6cded7fa5800b42
Toomas Tahves <toomastahves@hotmail.com>
2019-03-02 22:50:42 +0200

    added flask cors

--- RestService/app.py
+++ RestService/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes import app_
+from flask_cors import CORS
 from playhouse.postgres_ext import PostgresqlExtDatabase
 from models import database_proxy, create_tables
 
@@ -7,6 +8,7 @@ database = PostgresqlExtDatabase('remotelab', user='postgres', password='postgre
 database_proxy.initialize(database)
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_)
 
 if __name__ == '__main__':
