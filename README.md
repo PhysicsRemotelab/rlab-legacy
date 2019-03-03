3d751589ac1679ed2950f7411b9936e85f0f2cf2
Toomas Tahves <toomastahves@hotmail.com>
2019-03-03 19:15:47 +0200

    added website dev starter

--- README.md
+++ README.md
@@ -2,4 +2,8 @@ RestService:
 1. Create virtual environment: python -m venv venv
 2. Activate vnev in Windows: .\venv\Scripts\Activate.ps1
 3. Install dependencies: pip install -r requirements.txt
-4. Start application: python app.py (Requires active PostgeSQL database)
+4. Start application: python app.py (requires active PostgeSQL database)
+
+Website:
+1. Install dependencies: npm install
+2. Run development server: npm run dev
---
+++ Website/webpack/webpack.config.dev.js
@@ -0,0 +1,64 @@
+'use strict'
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  entry: [
+    './src/app.js'
+  ],
+  output: {
+    filename: 'bundle.js'
+  },
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        use: 'eslint-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'public/index.html',
+      inject: true
+    }),
+    new VueLoaderPlugin()
+  ]
+}
